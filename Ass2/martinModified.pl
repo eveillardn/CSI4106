90a1b0d3f71469ba7854b23c70475d26b2a4b749
Nicolas <eveillardn@gmail.com>
2013-11-07 16:32:34 -0500

    Finished Ass2
    Yay it's done. After 3 brain burnouts and a stupid amount of
    duplicated/messy code, the methods q1, q2, q3, and test1 work perfectly.
    We can clean up the code if you guys like but I'm glad that at least the
    answers are working. Basically there are the methods to generate a quick
    path, and to generate a cheap path. Our solutions will never look for
    optimization between the two. For example, for q1 where we want a trip
    under 15 hours, I simply made it give the quickest path (and the most
    expensive) and checked that the length of time was less then 15 hours.
    For q2 that wants a cheap trip, I generated the cheapest trip (walking
    everywhere) and made sure the cost was less then 2000$. Last question
    asks that we have the shortest trip (distance?), which is forced by the
    "cheap" and "quick" methods anyways. I decided to just use the quick
    method since it'll make the shortest in terms of distance and speed.

--- Ass2/martinModified.pl
+++ Ass2/martinModified.pl
@@ -19,61 +19,80 @@ route(A, B, Distance) :- edge(B, A, Distance).
 
 %Calculates the time and cost when travelling a Distance from Town.
 answerTimeAndCost(Time,Cost,Time,Cost).
-timeAndCost(Town,Distance, Time, Cost, plane) :- Distance >= 400,
+timeAndCost(Distance, Time, Cost, plane) :- Distance >= 400,
 	Time2 is Distance / 500, Cost2 is Distance, answerTimeAndCost(Time,Cost,Time2,Cost2).
-timeAndCost(Town,Distance, Time, Cost, train) :- Time2 is Distance / 120, Cost2 is 0.75 * Distance, answerTimeAndCost(Time,Cost,Time2,Cost2).
-timeAndCost(Town,Distance, Time, Cost, bus) :- Town \= a, Town \= e, Town \= f, Time2 is Distance / 80, Cost2 is 0.40 * Distance, answerTimeAndCost(Time,Cost,Time2,Cost2).
-timeAndCost(Town,Distance, Time, Cost, car) :- Time2 is Distance / 100, Cost2 is 0.60 * Distance, answerTimeAndCost(Time,Cost,Time2,Cost2).
-timeAndCost(Town,Distance, Time, Cost, walk) :- Time2 is Distance / 5, Cost2 is 0, answerTimeAndCost(Time,Cost,Time2,Cost2).
+timeAndCost(Distance, Time, Cost, train) :- Time2 is Distance / 120, Cost2 is 0.75 * Distance, answerTimeAndCost(Time,Cost,Time2,Cost2).
+timeAndCost(Distance, Time, Cost, bus) :- Town \= a, Town \= e, Town \= f, Time2 is Distance / 80, Cost2 is 0.40 * Distance, answerTimeAndCost(Time,Cost,Time2,Cost2).
+timeAndCost(Distance, Time, Cost, car) :- Time2 is Distance / 100, Cost2 is 0.60 * Distance, answerTimeAndCost(Time,Cost,Time2,Cost2).
+timeAndCost(Distance, Time, Cost, walk) :- Time2 is Distance / 5, Cost2 is 0, answerTimeAndCost(Time,Cost,Time2,Cost2).
 
 %Returns all possible Modes of transportations for this traveled Distance from given Town, along with their Cost, while sorted by Time
-quickTravel(Town,Distance,Solution) :-
-	setof([Time,Cost,Mode],timeAndCost(Town,Distance,Time,Cost,Mode),Solution).
+quickTravel(Distance,Solution) :-
+	setof([Time,Cost,Mode],timeAndCost(Distance,Time,Cost,Mode),Solution).
 %Only returns quickest possible movement
-quickestTravel(Town,Distance,Time,Cost,Mode):- quickTravel(Town,Distance,[[Time,Cost,Mode]|_]).
+quickestTravel(Distance,Time,Cost,Mode):- quickTravel(Distance,[[Time,Cost,Mode]|_]).
 
 %Returns all possible Modes of transportations for this traveled Distance from given Town, along with their Time, while sorted by Cost	
-cheapTravel(Town,Distance,Solution) :-
-	setof([Cost,Time,Mode],timeAndCost(Town,Distance,Time,Cost,Mode),Solution).
+cheapTravel(Distance,Solution) :-
+	setof([Cost,Time,Mode],timeAndCost(Distance,Time,Cost,Mode),Solution).
 %Only returns cheapest possible movement
-cheapestTravel(Town,Distance,Time,Cost,Mode):- cheapTravel(Town,Distance,[[Cost,Time,Mode]|_]).
+cheapestTravel(Distance,Time,Cost,Mode):- cheapTravel(Distance,[[Cost,Time,Mode]|_]).
 	
 %Returns a Solution displaying traveled nodes with the mode of transportation, the total Time of this solution, and the total Cost of this solution. Emphasis is on the speed and doesn't account for cost.
 getQuickTimeAndCost([_ | []], Solution, Solution, Time, Cost, Time, Cost).
-getQuickTimeAndCost([T1, T2 | Tail], TempSolution, Solution, TempTime, TempCost, Time, Cost) :- route(T1, T2, Distance), quickestTravel(Town,Distance, Time1, Cost1, Mode),
+getQuickTimeAndCost([T1, T2 | Tail], TempSolution, Solution, TempTime, TempCost, Time, Cost) :- route(T1, T2, Distance), quickestTravel(Distance, Time1, Cost1, Mode),
 	append(TempSolution, [T1, Mode], Sol2), append([T2], Tail, Path2), TempTime2 is TempTime + Time1, TempCost2 is TempCost + Cost1,
 	getQuickTimeAndCost(Path2, Sol2, Solution, TempTime2, TempCost2, Time, Cost).
 
 %Returns a Solution displaying traveled nodes with the mode of transportation, the total Time of this solution, and the total Cost of this solution. Emphasis is on the cost and doesn't account for speed.	
 getCheapTimeAndCost([_ | []], Solution, Solution, Time, Cost, Time, Cost).
-getCheapTimeAndCost([T1, T2 | Tail], TempSolution, Solution, TempTime, TempCost, Time, Cost) :- route(T1, T2, Distance), cheapestTravel(Town,Distance, Time1, Cost1, Mode),
+getCheapTimeAndCost([T1, T2 |Tail], TempSolution, Solution, TempTime, TempCost, Time, Cost) :- route(T1, T2, Distance), cheapestTravel(Distance, Time1, Cost1, Mode),
 	append(TempSolution, [T1, Mode], Sol2), append([T2], Tail, Path2), TempTime2 is TempTime + Time1, TempCost2 is TempCost + Cost1,
 	getCheapTimeAndCost(Path2, Sol2, Solution, TempTime2, TempCost2, Time, Cost).
 	
 %Generates a "Quick" solution from starting node S hitting at least all Towns specified exactly once and going back to s
-generateQuick(S, Towns, S1, Distance, Time, Cost) :- findPath(S, S, Path, Distance), submember(Towns, Path), 
-	setof([Distance,S1,Time,Cost],getQuickTimeAndCost(Path, [], S1, 0, 0, Time, Cost),Set).
+generateQuick(S, Towns, Solution, Distance, Time, Cost) :- 
+	setof([Path,Distance1],findPath(S, S, Path, Distance1,Towns),Set),
+	allElemsCheckQuick(WithLenghts,[],Set),
+	setof([Time1,S1,Cost1,Distance1],allElemsQuick(WithLenghts,Time1,S1,Cost1,Distance1),[[Time,Solution,Cost,Distance]|_]).
 
 %Generates a "Cheap" solution from starting node S hitting at least all Towns specified exactly once and going back to s
 generateCheap(S, Towns, Solution, Distance, Time, Cost) :- 
-	setof([Distance,Path],findPath(S, S, Path, Distance),Set),
-	plausible(Set,[],Best,Towns),
-	setof([Cost,S1,Time,Distance],getCheapTimeAndCost(Best, [], S1, 0, 0, Time, Cost),[Solution|_]).	
+	setof([Path,Distance1],findPath(S, S, Path, Distance1,Towns),Set),
+	allElemsCheckCheap(WithLenghts,[],Set),
+	setof([Cost1,S1,Time1,Distance1],allElemsCheap(WithLenghts,Cost1,S1,Time1,Distance1),[[Cost,Solution,Time,Distance]|_]).
 	
-plausible([],Answer,Answer,_).
-plausible([[Solution|_]|T],TempSol,Answer,Towns):-
-	subset(Towns,Solution),
-	append(TempSol, [Solution], NewSol),
-	!,
-	NewSol is TempSol,
-	plausible(T,NewSol,Answer,Towns).
+%Sorry about the horrible name, this method takes prebuild lists of the form [[Path, Distance], [Path, Distance],...] and adds Costs and Times to these paths
+allElemsCheckCheap(Solution,Solution,[]).
+allElemsCheckCheap(Solution,TempSolution,[[H,Distance]|T]):-
+	getCheapTimeAndCost(H,[],S1,0,0,Time,Cost),
+	append(TempSolution,[[Cost,S1,Time,Distance]],NewTemp),
+	allElemsCheckCheap(Solution,NewTemp,T).
+
+allElemsCheckQuick(Solution,Solution,[]).
+allElemsCheckQuick(Solution,TempSolution,[[H,Distance]|T]):-
+	getQuickTimeAndCost(H,[],S1,0,0,Time,Cost),
+	append(TempSolution,[[Time,S1,Cost,Distance]],NewTemp),
+	allElemsCheckQuick(Solution,NewTemp,T).
+	
+%Again, a horrible name and a very sketchy method. This method is used as a very lazy way to sort a list of lists containing many elements using the setof() command.
+allElemsCheap([[Cost,Path,Time,Distance]|[]],Cost,Path,Time,Distance).	%Happens upon last element
+allElemsCheap([[Cost,Path,Time,Distance]|_],Cost,Path,Time,Distance).	%Happens on every element but the last one
+allElemsCheap([_|T],Cost,Path,Time,Distance):-							%Happens as long as the list isn't empty
+	allElemsCheap(T,Cost,Path,Time,Distance).
+	
+allElemsQuick([[Time,Path,Cost,Distance]|[]],Time,Path,Cost,Distance).	%Happens upon last element
+allElemsQuick([[Time,Path,Cost,Distance]|_],Time,Path,Cost,Distance).	%Happens on every element but the last one
+allElemsQuick([_|T],Time,Path,Cost,Distance):-							%Happens as long as the list isn't empty
+	allElemsQuick(T,Time,Path,Cost,Distance).
 	
 %Makes a trip from town A to town B while saving the visited towns (path) and the total distance.
-trip(A, B, Path, [B | Path], Distance) :-
-	route(A, B, Distance).
-trip(A, B, PreviousPath, NewPath, Distance) :-
+trip(A, B, Path, [B | Path], Distance,Towns) :-
+	route(A, B, Distance),
+	subset(Towns,[B|Path]).
+trip(A, B, PreviousPath, NewPath, Distance,Towns) :-
 	route(A, C, D1), B \= C, \+(member(C, PreviousPath)), 
-	trip(C, B, [C | PreviousPath], NewPath, D2), Distance is D1 + D2.
+	trip(C, B, [C | PreviousPath], NewPath, D2, Towns), Distance is D1 + D2.
 
 %Reverses a list. reverse([a, b, c], L): L = [c, b, a].
 reverseList(List, ReversedList) :- reverseList(List, [], ReversedList).
@@ -82,8 +101,8 @@ reverseList([H | T], Accumulator, ReversedList) :-
 	reverseList(T, [H | Accumulator], ReversedList).
 
 %Finds a path from town A to town B.
-findPath(A, B, Path, Distance) :-
-	trip(A, B, [A], ReversedPath, Distance), reverseList(ReversedPath, Path).
+findPath(A, B, Path, Distance,Towns) :-
+	trip(A, B, [A], ReversedPath, Distance,Towns), submember(Towns, ReversedPath), reverseList(ReversedPath, Path).
 
 %Appends the second list to the first list.
 append([], L, L).
@@ -96,32 +115,28 @@ submember([H | T], L2) :- member(H, L2), !, submember(T, L2).
 print([]).
 print([H | T]) :- write(H), write(' '), print(T).
 
-%Question 1:
-%tripMaxTime(S, Towns, MaxTime) :- generateFastest(S, Towns, Solution, Distance, Time, Cost, MaxTime), Time < MaxTime, printTrip(Solution, Distance, Time, Cost).
-%q1 :- tripMaxTime(s, [a], 15).
+%Question 1: 
+q1:-
+	setof([Solution,Distance,Time,Cost],generateQuick(s,[b,e,c],Solution,Distance,Time,Cost),[[S,D,T,C]|_]),
+	Time @< 15,
+	printTrip(S,D,T,C).
 
 %Question 2:
-%tripMaxCost(S, Towns, MaxCost) :- generateCheapest(S, Towns, Solution, Distance, Time, Cost, MaxCost), Cost < MaxCost, printTrip(Solution, Distance, Time, Cost).
-%q2 :- tripMaxCost(s, [a,b,c,d,e,f,g], 2000).
+q2:-
+	setof([Solution,Distance,Time,Cost],generateCheap(s,[a,b,c,d,e,f,g],Solution,Distance,Time,Cost),[[S,D,T,C]|_]),
+	Cost @< 2000,
+	printTrip(S,D,T,C).
 
-%Question 3 Build a set of solutions and return element going from Start to Start while including Including nodes in the shortest distance
-q3(Start,Including):- setof([D,A],generate(Start,Including,A,D,_,_),[Solution|_]), print(Solution).
-
-%Path solver includes round trip from s to s every time
-pathSolver(S, Towns, Path, Distance) :- 
-	findPath(S, S, Path, Distance), submember(Towns, Path).
-
-%See full output from q3
-q3test(Start,Including):- setof([D,A],pathSolver(Start,Including,A,D),Set), printAll(Set).
-
-%print ever element in a set
-printAll([]).
-printAll([H|T]) :-
-	print(H),nl,
-	printAll(T).
+%Question 3:
+q3:-
+	setof([Solution,Distance,Time,Cost],generateQuick(s,[c],Solution,Distance,Time,Cost),[[S,D,T,C]|_]),
+	printTrip(S,D,T,C).
+	
+%SanityTest This path should be at max either [s,a,d,s] or [s,d,a,s]
+test1:-
+	setof([Solution,Distance,Time,Cost],generateQuick(s,[a],Solution,Distance,Time,Cost),[[S,D,T,C]|_]),
+	printTrip(S,D,T,C).
 
 %Prints a path, its distance, time and cost.
-printTrip([Path, _, _, _], Distance, Time, Cost) :- print(Path), write('s'), nl, write('Total distance: '), write(Distance), write('km.'),
+printTrip(Path, Distance, Time, Cost) :- print(Path), write('s'), nl, write('Total distance: '), write(Distance), write('km.'),
 	nl, write('Total time: '), write(Time), write('h.'), nl, write('Total cost: $'), write(Cost), write('.').
-
-
